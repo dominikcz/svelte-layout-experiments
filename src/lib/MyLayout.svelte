5c3a04bcb39de7986ac43caffdfb44a30f9a0a74
Dominik Czechowski <dominik.czechowski@mccomp.pl>
2025-03-16 10:34:00 +0100

    ts version with Snippets

--- src/lib/MyLayout.svelte
+++ src/lib/MyLayout.svelte
@@ -1,37 +1,41 @@
-<script>
+<script lang="ts">
+	import type { Snippet } from "svelte";
 
+	interface LayoutProps {
+		children?: Snippet;
+	}
+	let { children }: LayoutProps = $props();
 </script>
 
 <div>
-    <nav>
-        <a href="/page1/">page 1</a>
-        <a href="/page2/">page 2</a>
-        <a href="/page3/">page 3</a>
-    </nav>
-    <main>
-        <slot />
-    </main>
+	<nav>
+		<a href="/page1/">page 1</a>
+		<a href="/page2/">page 2</a>
+		<a href="/page3/">page 3</a>
+	</nav>
+	<main>
+		{@render children?.()}
+	</main>
 </div>
 
 <style>
-    div{
-        background-color: rebeccapurple;
-        display: flex;
-        flex-flow: row nowrap;
-        padding: 1rem;
-        justify-items: stretch;
-    }
-    nav{
-        background-color: tomato;
-        display: flex;
-        flex-flow: column nowrap;
-        gap: 1rem;
-        padding: 1rem;
-
-    }
-    main{
-        background-color: gold;
-        flex-grow: 1;
-        padding: 1rem;
-    }
+	div {
+		background-color: rebeccapurple;
+		display: flex;
+		flex-flow: row nowrap;
+		padding: 1rem;
+		justify-items: stretch;
+	}
+	nav {
+		background-color: tomato;
+		display: flex;
+		flex-flow: column nowrap;
+		gap: 1rem;
+		padding: 1rem;
+	}
+	main {
+		background-color: gold;
+		flex-grow: 1;
+		padding: 1rem;
+	}
 </style>
