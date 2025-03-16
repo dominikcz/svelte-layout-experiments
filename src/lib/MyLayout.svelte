ea856e2e6a1ce57f2856658f359ef2e070ab498b
Dominik Czechowski <dominik.czechowski@mccomp.pl>
2025-03-16 10:27:21 +0100

    updates after migration

--- src/lib/MyLayout.svelte
+++ src/lib/MyLayout.svelte
@@ -4,9 +4,9 @@
 
 <div>
     <nav>
-        <a href="/page1">page 1</a>
-        <a href="/page2">page 2</a>
-        <a href="/page3">page 3</a>
+        <a href="/page1/">page 1</a>
+        <a href="/page2/">page 2</a>
+        <a href="/page3/">page 3</a>
     </nav>
     <main>
         <slot />
